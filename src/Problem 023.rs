74940fff2f0b6ee9e2a39265b0cb38028d77adf0
listless <listless@hotmail.com>
2014-02-06 23:05:49 +0000

    Cleaning up solutions

--- src/Problem 023.rs
+++ src/Problem 023.rs
@@ -32,7 +32,7 @@ fn sum_of_proper_divisors(number: int) -> int {
 	if number == 1 {
 		0
 	} else {
-		let mut sum: int = - number;
+		let mut sum: int = -number;
 
 		for i in count(1, 1).take_while(|&i| i * i <= number) {
 			if number % i == 0 {
